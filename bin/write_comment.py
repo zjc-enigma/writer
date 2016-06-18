fd36ea2dfba00a9e6ad0ba16298df7d7a3fb9cd3
zjc-enigma <enigma@pku.edu.cn>
2016-06-18 15:48:51 +0800

    begin to make a writer bot

---
+++ bin/write_comment.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+import sys
+sys.path.append('..')
+import os
+from utils import myutils
+
+greeting_path = "../data/greeting"
+greeting_list = myutils.read_line_to_list(greeting_path)
+
+
+
+
+
+comment = "{greeting}"
+
